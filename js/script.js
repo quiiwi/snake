3c669773907951d9788b91ebde694d36330b078b
quiiwi <kilic.aslan@lepoles.com>
2018-07-10 14:34:39 +0200

    creer queue

--- js/script.js
+++ js/script.js
@@ -1,72 +1,89 @@
-// var snake = document.querySelector('#snake');
+var snake = document.querySelector('#snake');
         
-//     var directionHB = 0;
-//     var directionGD = 0;
-//     var deplacementBas = 'jebougeverslebas $';
-//     var deplacementHaut = 'jebougeversleHaut $';
-//     var deplacementGauche = 'jebougeverslaGauche $';
-//     var deplacementDroite = 'jebougeverslaDroite $';
-//     var vers = 'null';
+    var directionHB = 0;
+    var directionGD = 0;
+    var deplacementBas = 'jebougeverslebas $';
+    var deplacementHaut = 'jebougeversleHaut $';
+    var deplacementGauche = 'jebougeverslaGauche $';
+    var deplacementDroite = 'jebougeverslaDroite $';
+    var vers = deplacementDroite;
 
-//     function moveGlob(){
-//         console.log(vers,directionHB,directionGD);
+    document.querySelector('#lancer').addEventListener('click', function() {
 
+        function moveGlob(){
 
-//         if (vers === deplacementBas){
-//             directionHB += +5;
-//             snake.style.top = directionHB + 'px';
-//         } else if (vers === deplacementGauche){
-//             directionGD += -5;
-//             snake.style.left = directionGD +'px';
-//         } else if (vers === deplacementDroite){
-//             directionGD += +5;
-//             snake.style.left = directionGD + 'px';
-//         }else if (vers === deplacementHaut){
-//             directionHB += -5;
-//             snake.style.top = directionHB + 'px';
-//         }
-//         if(directionHB>=500 || directionGD>=500){
-//             clearInterval(boucle);
-//         }
-//         if(directionHB<0 || directionGD<0){
-//             clearInterval(boucle);
-//         }
-//     }
+            console.log(vers,directionHB,directionGD);
 
+            if (vers === deplacementBas){
+                directionHB += +5;
+                snake.style.top = directionHB + 'px';
+            } else if (vers === deplacementGauche){
+                directionGD += -5;
+                snake.style.left = directionGD +'px';
+            } else if (vers === deplacementDroite){
+                directionGD += +5;
+                snake.style.left = directionGD + 'px';
+            }else if (vers === deplacementHaut){
+                directionHB += -5;
+                snake.style.top = directionHB + 'px';
+            }
+            
+            if(directionHB>=500 || directionGD>=500){
+                clearInterval(boucle);
+            }
+            if(directionHB<0 || directionGD<0){
+                clearInterval(boucle);
+            }
 
-//     var boucle = setInterval(moveGlob, 10);
+            creerqueue();
+        }
 
-//     document.onkeydown = function(e){
-//         switch(e.keyCode){
-//             case 37:
-//                 vers = deplacementGauche;
-//             break;
-//             case 38:
-//                 vers = deplacementHaut;
-//             break;
-//             case 39:
-//                 vers = deplacementDroite;
-//             break;
-//             case 40:
-//                 vers = deplacementBas;
-//             break;
-//         }
-//     }
+        var boucle = setInterval(moveGlob, 1000);
 
-
-var i = 0;
-
-
-
-document.querySelector('#lancer').addEventListener('click', function() {
-    i++;
-    console.log(i);
+        function creerqueue(){
+            var newDiv = document.createElement('div');
+            newDiv.setAttribute('class','queue');
+            newDiv.setAttribute('top', directionHB);
+            var currentDiv = document.getElementById('frillage');
+            document.body.insertBefore(newDiv,currentDiv);
+        }
+    });
     
-});
+
+
+   
+
+    document.onkeydown = function(e){
+        switch(e.keyCode){
+            case 37:
+                vers = deplacementGauche;
+            break;
+            case 38:
+                vers = deplacementHaut;
+            break;
+            case 39:
+                vers = deplacementDroite;
+            break;
+            case 40:
+                vers = deplacementBas;
+            break;
+        }
+    }
+
+
+// var i = 0;
 
 
 
-var boucle = setInterval(maFonction, 1000); 
+// document.querySelector('#lancer').addEventListener('click', function() {
+//     i++;
+//     console.log(i);
+    
+// });
+
+
+
+// var boucle = setInterval(maFonction, 1000); 
 
 
 
@@ -74,7 +91,7 @@ var boucle = setInterval(maFonction, 1000);
 
 
 
-function creerSnake(){
-    var newDiv = document.createElement('div');
-    newDiv.setAttribute('class', 'queue$');
-}
+// function creerSnake(){
+//     var newDiv = document.createElement('div');
+//     newDiv.setAttribute('class', 'queue$');
+// }
