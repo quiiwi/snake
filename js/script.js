10adc00241db77a2abc4e4f75a514980c29f13ad
quiiwi <kilic.aslan@lepoles.com>
2018-07-16 16:09:14 +0200

    16/07

--- js/script.js
+++ js/script.js
@@ -12,15 +12,20 @@
     var vers = deplacementDroite;
     var Q = 15;
     var xFinal, yFinal;
+    var score = 0;
+    var vitesseDuJeu = 100;
+    var positionDesQueues = [];
+
+    
 
     //LA FONCTION PRINCIPALE
-
     document.querySelector('#lancer').addEventListener('click', function() {
 
-        function moveGlob(){ //ma fonction pour bouger le serpent
+        //LA FONCTION POUR BOUGER LE SERPENT
+        function moveGlob(){ 
+            console.log(vitesseDuJeu);
 
-
-            console.log(vers,directionHB,directionGD); //TEST
+            // console.log(vers,directionHB,directionGD);
 
             if (vers === deplacementBas){ 
                 directionHB += +5;
@@ -37,42 +42,89 @@
             }
             
             if(directionHB>=500 || directionGD>=500){
-                clearInterval(boucle); //lorsque je sors de la grille de jeu la boucle principale s'arrête
+                clearInterval(vitessDebutant); //lorsque je sors de la grille de jeu la boucle principale s'arrête
+                clearInterval(vitessPro);
+                clearInterval(vitessMaster);
+
             }
             if(directionHB<0 || directionGD<0){
-                clearInterval(boucle); //lorsque je sors de la grille de jeu la boucle principale s'arrête
+                clearInterval(vitessDebutant); //lorsque je sors de la grille de jeu la boucle principale s'arrête
+                clearInterval(vitessPro);
+                clearInterval(vitessMaster);
             }
 
             if(directionHB == yFinal && directionGD == xFinal){
+
                 creerpomme();
+
                 Q += 10;
+
                 $('.pomme:first').remove();
+
+                score += 1000;
+
             }
 
-            
-            
             creerqueue();
+
             supprimequeue();
 
-            
 
-            //return x = directionHB;
+            $('#score').text(score);
+
+
+            // console.log(score);
         }
+        
 
-        var boucle = setInterval(moveGlob, 50);
 
+        if (score == 0){
+            
+            mavitessdebutant();
+                
+        } else if(score == 1000){
+            
+            mavitesspro();
+
+        } else if(score >= 2000){
+
+            mavitessmaster();
+            
+        }
+        
+
+        function mavitessdebutant(){
+            var vitessDebutant = setInterval(moveGlob, 100);
+        }
+        
+        function mavitesspro(){
+            clearInterval(vitessDebutant);
+
+            var vitessPro = setInterval(moveGlob,50);
+        }
+        
+        function mavitessmaster(){
+            clearInterval(vitessPro);
+            var vitessMaster = setInterval(moveGlob,10);
+        }
+        
+
+     
+
+            
+        
+
+        
 
 
 
         //LA FONCTION CREER LA QUEUE DU SERPENT
-
         function creerqueue(){ //ma fonction qui créer une .queue
             var newDiv = document.createElement('div'); // créer une nouvelle div
             newDiv.setAttribute('class','queue'); //lui donner une class .queue
             newDiv.style.left = directionGD + 'px' ; //lui attribue la position par rapport au left
             newDiv.style.top = directionHB + 'px' ; //lui attribue la position par rapport au top
-            // newDiv.setAttribute('font-size',7 +'px'); //lui donne une valeur de i = 7
-            // newDiv.setAttribute('i', -1);
+
             grillage.appendChild(newDiv); //indique que newdiv(".queue") est un enfant de #grillage
 
 
@@ -81,30 +133,26 @@
         }
 
         document.querySelector('#stopper').addEventListener('click', function() {
-            clearInterval(boucle); // le bouton pour arrêter le jeu
+            clearInterval(vitessDebutant); // le bouton pour arrêter le jeu
+            clearInterval(vitessPro);
+            clearInterval(vitessMaster);
         });
 
 
         //LA FONCTION SUPPRIMER LA QUEUE DU SERPENT
-
         function supprimequeue(){
-        //     var Q = document.querySelectorAll(".queue").fontSize;
             Q--; //Q est initialement == à 10, à chaque tour de boucle Q - 1 et lorsque Q == 1 on supprime la première div qui a été crée
-            console.log(Q);
+            // console.log(Q);
             if(Q==1){
                 Q++;
                 $('.queue:first').remove(); // C'est plus facile en jQuery...... 
             }
         }
 
-
-
-       
     });
     
 
     // EVENEMENTS AUX TOUCHES DU CLAVIER
-
     document.onkeydown = function(e){
         switch(e.keyCode){
             case 37: //touche gauche
@@ -135,22 +183,21 @@
         var random = Math.random()*1000000000;
         var randomEntier = random | 0;
         randomEntierString = randomEntier.toString();
-        console.log(random);
-        console.log(randomEntier);
-        console.log(randomEntierString[3],randomEntierString[4],randomEntierString[5],randomEntierString[6]);
+        // console.log(random);
+        // console.log(randomEntier);
+        // console.log(randomEntierString[3],randomEntierString[4],randomEntierString[5],randomEntierString[6]);
 
         var x1 = randomEntierString[3];
         var x2 = randomEntierString[4];
 
         var y1 = randomEntierString[5];
         var y2 = randomEntierString[6];
-
-        console.log(x1,x2,y1,y2);
+        // console.log(x1,x2,y1,y2);
 
         var x = x1 + x2;
-        console.log(x);
+        // console.log(x);
         var y = y1 + y2;
-        console.log(y);
+        // console.log(y);
 
         xFinal = x*5;
         yFinal = y*5;
@@ -161,8 +208,7 @@
         newDiv2.setAttribute('class','pomme'); //lui donner une class .queue
         newDiv2.style.left = xFinal + 'px' ; //lui attribue la position par rapport au left
         newDiv2.style.top = yFinal + 'px' ; //lui attribue la position par rapport au top
-        // newDiv.setAttribute('font-size',7 +'px'); //lui donne une valeur de i = 7
-        // newDiv.setAttribute('i', -1);
+
         grillage.appendChild(newDiv2); //indique que newdiv(".queue") est un enfant de #grillage
 
 
@@ -172,28 +218,6 @@
     };
     
     creerpomme();
-    console.log(xFinal, yFinal);
-// var i = 0;
 
 
-
-// document.querySelector('#lancer').addEventListener('click', function() {
-//     i++;
-//     console.log(i);
-    
-// });
-
-
-
-// var boucle = setInterval(maFonction, 1000); 
-
-
-
-
-
-
-
-// function creerSnake(){
-//     var newDiv = document.createElement('div');
-//     newDiv.setAttribute('class', 'queue$');
-// }
+    //Script by Quiiwi
